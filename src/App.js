5160fbb27215d89cd8b2b810ae841ca2d1cbc2bc
Leonnardo Nascimento <leonnardoenriquesn@gmail.com>
2022-01-18 12:18:04 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
 
-import PersonCard from './components/MyNewComponent';
+import UserForm from './components/MyNewComponent';
 
 function App() {
   return (
     <div className="App">
-      <PersonCard firstName="Jane" lastName="Doe" age={45} hair={"Black"}/>
-      <PersonCard firstName="John" lastName="Smith" age={88} hair={"Brown"}/>
-      <PersonCard firstName="Millard" lastName="Fillmore" age={50} hair={"Black"}/>
-      <PersonCard firstName="Maria" lastName="Smith" age={62} hair={"Brown"}/>
+      <UserForm />
     </div>
   );
 }
--- src/components/MyNewComponent.js
+++ src/components/MyNewComponent.js
@@ -1,12 +1,54 @@
-import React from 'react';
-const PersonCard = props => {
+import React, { useState } from  'react';
+    
+    
+const UserForm = (props) => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [Cpassword, setCPassword] = useState("");
+    
+    const createUser = (e) => {
+        e.preventDefault();
+        const newUser = { firstName,lastName, email, password, Cpassword };
+        console.log("Welcome", newUser);
+    };
+    
     return(
-        <div>
-            <h1>{ props.lastName }, { props.firstName }</h1>
-            <p>Age: { props.age }</p>
-            <p>Hair Color: { props.hairColor }</p>
-        </div>
-    );
-}
-export default PersonCard;
+    <div>
+        <form onSubmit={ createUser }>
+            <div>
+                <label>First Name: </label> 
+                <input type="text" onChange={ (e) => setFirstName(e.target.value) } />
+            </div>
+            <div>
+                <label>Last Name: </label> 
+                <input type="text" onChange={ (e) => setLastName(e.target.value) } />
+            </div>
+            <div>
+                <label>Email Address: </label> 
+                <input type="text" onChange={ (e) => setEmail(e.target.value) } />
+            </div>
+            <div>
+                <label>Password: </label>
+                <input type="text" onChange={ (e) => setPassword(e.target.value) } />
+            </div>
+            <div>
+                <label>Confirm password: </label>
+                <input type="text" onChange={ (e) => setCPassword(e.target.value) } />
+            </div>
+            <input type="submit" value="Create User" />
+        </form>
 
+        <p> {firstName} </p>
+        <p> {lastName} </p>
+        <p> {email} </p>
+        <p> {password} </p>
+        <p> {Cpassword} </p>
+
+    </div>
+
+    );
+};
+    
+export default UserForm;
