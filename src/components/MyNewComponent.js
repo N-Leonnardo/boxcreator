589285ff490a783803964977d66dbead86229945
Leonnardo Nascimento <leonnardoenriquesn@gmail.com>
2022-01-17 16:35:58 -0800

    initial commit

---
+++ src/components/MyNewComponent.js
@@ -0,0 +1,21 @@
+import React, { Component} from 'react';
+
+class MyNewComponent extends Component{
+    render(){
+        return(
+            <div> 
+                <h1>
+                    {this.props.lastName} , {this.props.firstName}
+                </h1>
+                <p>
+                    Age : {this.props.age}
+                </p>
+                <p>
+                    Hair Color : {this.props.hair}
+                </p>
+            </div>
+        )
+    }
+}
+
+export default MyNewComponent
