05fd4f48eaa2144cd554ed7aaadb7236db9ed573
Leonnardo Nascimento <leonnardoenriquesn@gmail.com>
2022-01-18 00:27:05 -0800

    initial commit

--- src/components/MyNewComponent.js
+++ src/components/MyNewComponent.js
@@ -1,6 +1,20 @@
 import React, { Component} from 'react';
 
 class MyNewComponent extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            age :  this.props.age
+        };
+    }
+
+
+    incrementage() {
+        this.setState({
+            age : this.state.age + 1
+        })
+    }
+
     render(){
         return(
             <div> 
@@ -8,11 +22,13 @@ class MyNewComponent extends Component{
                     {this.props.lastName} , {this.props.firstName}
                 </h1>
                 <p>
-                    Age : {this.props.age}
+                    Age : {this.state.age}
                 </p>
                 <p>
                     Hair Color : {this.props.hair}
                 </p>
+                <button onClick={ ()=> this.incrementage() }>Add birthday to {this.props.firstName} </button>
+
             </div>
         )
     }
